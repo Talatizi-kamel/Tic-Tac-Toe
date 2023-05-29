c16dd37e56659d93768285e0806b8a456d843ee3
talatizi kamel <kamel.talatizi@alumni.univ-avignon.fr>
2023-05-29 16:30:28 +0200

    interface de l accueil

---
+++ Morpion/src/main/java/com/example/morpion/AcceuilController.java
@@ -0,0 +1,47 @@
+package com.example.morpion;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+
+
+public class AcceuilController {
+
+    @FXML
+    Button button;
+    @FXML
+    AnchorPane anchorpane;
+    @FXML
+    Button button2;
+
+    public void initialize() {
+        // Initialisation des éléments de l'interface utilisateur ici
+        button2.setMaxWidth(Double.MAX_VALUE);
+        VBox.setVgrow(button2, Priority.ALWAYS);
+
+        VBox container = new VBox(button2);
+        container.setFillWidth(true);
+        anchorpane.getChildren().add(container);
+        // Positionne le VBox en bas à droite de l'AnchorPane
+        anchorpane.setBottomAnchor(container, 10.0);
+        anchorpane.setRightAnchor(container, 10.0);
+
+
+        button.setMaxWidth(Double.MAX_VALUE);
+        VBox.setVgrow(button, Priority.ALWAYS);
+
+        VBox container1 = new VBox(button);
+        container1.setFillWidth(true);
+        anchorpane.getChildren().add(container1);
+        // Positionne le VBox en bas à droite de l'AnchorPane
+        anchorpane.setBottomAnchor(container1, 10.0);
+        anchorpane.setLeftAnchor(container1, 10.0);
+
+
+    }
+}
